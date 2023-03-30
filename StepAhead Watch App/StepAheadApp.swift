89d28461dc288c8cec79336d37749673d25ec441
Jason Mai <maijason112102@gmail.com>
2023-03-30 17:03:31 -0400

    Initial Commit

---
+++ StepAhead Watch App/StepAheadApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepAheadApp.swift
+//  StepAhead Watch App
+//
+//  Created by Jason Mai on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct StepAhead_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
